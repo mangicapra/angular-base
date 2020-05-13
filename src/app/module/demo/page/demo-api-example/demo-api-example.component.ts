196966c7aa235c352dbc2acb3cac0f38db5a2fb1
dejo <d.dejanovic@dvcsolutions.com>
2020-05-13 09:45:48 +0200

    fix: Dropdown search implementation to demo module

--- src/app/module/demo/page/demo-api-example/demo-api-example.component.ts
+++ src/app/module/demo/page/demo-api-example/demo-api-example.component.ts
@@ -14,6 +14,8 @@ export class DemoApiExampleComponent implements OnInit, OnDestroy {
   public perPage = 10;
   public p = 1;
   public authors: Author[];
+  public searchList: { name: string; id: string }[];
+  public selectedVal: string;
   public pages = [
     { value: 1, name: '10' },
     { value: 2, name: '20' },
@@ -40,6 +42,7 @@ export class DemoApiExampleComponent implements OnInit, OnDestroy {
       .findAll(Author, {
         include: 'books,photos',
         page: { size: this.perPage, number: this.p },
+        filter: { name: this.selectedVal },
       })
       .pipe(takeUntil(this.getAuthors$))
       .subscribe((authors: JsonApiQueryData<Author>) => {
@@ -55,6 +58,24 @@ export class DemoApiExampleComponent implements OnInit, OnDestroy {
 
   handlePageChange(ev): void {
     this.p = ev === '' ? 1 : ev;
+    this.getAuthors();
+  }
+
+  handleEnteredTerm(name): void {
+    this.store
+      .findAll(Author, {
+        filter: { name },
+      })
+      .subscribe((authors: JsonApiQueryData<Author>) => {
+        this.searchList = authors
+          .getModels()
+          .map((author: Author) => ({ name: author.name, id: author.id }));
+      });
+  }
+
+  handleDropdownSearchSelect(ev): void {
+    this.selectedVal = ev.name;
+    this.p = 1;
     this.getAuthors();
   }
 }
